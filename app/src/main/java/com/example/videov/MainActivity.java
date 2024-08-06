126b343af3887208e55740c6a137de040ede93d5
mitacsc <mayurkhalate63@gmail.com>
2024-08-06 14:42:52 +0530

    comit

---
+++ app/src/main/java/com/example/videov/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.videov;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        VideoView v=findViewById(R.id.video);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.sample);
+        v.setVideoURI(uri);
+        MediaController mediaController=new MediaController(this);
+        mediaController.setMediaPlayer(v);
+        v.setMediaController(mediaController);
+        v.start();
+
+
+
+    }
+}
